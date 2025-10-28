85e98988a266aa014e85ff6c727298960bc1092b
AODINGLVP <812206870@qq.com>
2025-10-28 20:11:54 +0000

    little change ,the basic of great loop

--- WM908_15_mywork/WM908_15_mywork.cpp
+++ WM908_15_mywork/WM908_15_mywork.cpp
@@ -35,23 +35,25 @@ int scvcount = 0;
 #include "enemiesmanager.h"
 #include "Water.h"
 
-bool scvtest=true;
+bool scvtest = true;
 void draw_object(GamesEngineeringBase::Window& canvas, GameObject** obj, int count);
 void draw_title(int x, int y, GamesEngineeringBase::Window& canvas, GamesEngineeringBase::Image& image);
-void draw_entire_background(int **mapsave1, GamesEngineeringBase::Window& canvas, GamesEngineeringBase::Image* tiles,int** offestmapx,int** offextmapy,Water** watermap);
+void draw_entire_background(int** mapsave1, GamesEngineeringBase::Window& canvas, GamesEngineeringBase::Image* tiles, int** offestmapx, int** offextmapy, Water** watermap);
 void controlHero(Hero& hero, GamesEngineeringBase::Window& canvas, float move);
 void draw_collision(GameObject& gameobject, GamesEngineeringBase::Window& canvas);
 void camera_draw(float x, float y, GamesEngineeringBase::Image& image, GamesEngineeringBase::Window& canvas, GameObject* obj);
-void changemao(Hero& hero, int** mapmapoffestx,int** mapoffesty);
+void changemao(Hero& hero, int** mapmapoffestx, int** mapoffesty);
 using namespace std;
 
 int main() {
-	Water** watermap = new Water* [42]();
+	int stage = 1;
+
+	Water** watermap = new Water * [42]();
 	for (int i = 0; i < 42; i++) {
 		watermap[i] = new Water[42]();
 	}
 	int** mapoffestx = new int* [100]();
-	for(int i=0;i<100;i++)
+	for (int i = 0; i < 100; i++)
 		mapoffestx[i] = new int[100]();
 	int** mapoffesty = new int* [100]();
 	for (int i = 0; i < 100; i++)
@@ -59,11 +61,11 @@ int main() {
 	HRESULT hr = CoInitialize(NULL); //  初始化 COM
 
 	GamesEngineeringBase::Timer timer;
-	GameObjectManager& gameobjectmanager=GameObjectManager::getInstance();
+	GameObjectManager& gameobjectmanager = GameObjectManager::getInstance();
 	Bulletmanager& bulletmanager = Bulletmanager::getInstance();
 	ifstream file("../Resources/tiles.txt");
 	Hero& hero = Hero::getInstance();
-	
+
 	enemiesmanager& enemymanager = enemiesmanager::getInstance();
 	Camera& camera = Camera::GetCamera();
 	camera.SetPosition(hero.transform.GetPositionX(), hero.transform.GetPositionY());
@@ -82,18 +84,18 @@ int main() {
 		}
 	}*/
 
-	
+
 	string line;
-	for (int linenumber = 0; getline(file, line);) {  
-		
+	for (int linenumber = 0; getline(file, line);) {
+
 		if (linenumber >= 6) {
-			
+
 			int columnline = 0;
 			int stack = -1;
 			for (int i = 0; i < line.size(); i++) {
 				if (line[i] == ',') {
 					mapsave1[linenumber - 6][columnline] = stack;
-					
+
 					columnline++;
 					stack = -1;
 				}
@@ -105,13 +107,13 @@ int main() {
 						stack = stack * 10 + (line[i] - '0');
 					}
 				}
-				
+
 			}
 		}
 		linenumber++;
-		
+
 	}
-	
+
 	GamesEngineeringBase::Image tiles[24];
 	for (int i = 0; i < 24; i++) {
 		tiles[i].load("../Resources/" + to_string(i) + ".png");
@@ -128,62 +130,88 @@ int main() {
 
 	GamesEngineeringBase::Image image;
 	GamesEngineeringBase::Window canvas;
-	
+
 	image.load("../Resources/A.png");
 	// Create a canvas window with dimensions 1024x768 and title “Tiles"
-	
 
-	
+
+
 	unsigned int planeX = 300;  // Initial x-coordinate for the plane image.
 	unsigned int planeY = 300;
 
 	canvas.create(1280, 1280, "Tiles");
-	hero.transform.SetPosition(canvas.getWidth() / 2, canvas.getHeight()/2);
+	hero.transform.SetPosition(canvas.getWidth() / 2, canvas.getHeight() / 2);
 	bool running = true; // Variable to control the main loop's running state.
 	cout << canvas.getWidth() << "   " << canvas.getHeight();
+	while (1) {
+		break;
+		if (stage == 1) {
+			
+
+		}
+		else if (stage == 2) {
+
+		}
+		else if (stage == 3) {
+
+		}
+		else if (stage == 4) {
+
+		}
+		else if (stage == 5) {
+		}
+		else if (stage == 6) {
+		}
+		else if (stage == 7) {
+
+		}
+	}
+
+
+
 	while (running)
 	{
-		
+
 		dt = timer.dt();
 		hero.updataability(dt);
 		fpsdtcount += dt;
 		fps++;
-		if(fpsdtcount >= 1.0f) {
+		if (fpsdtcount >= 1.0f) {
 			cout << "FPS: " << fps << endl;
 			fps = 0;
 			fpsdtcount = 0;
 		}
 		if (canvas.keyPressed(VK_ESCAPE)) break;
-		hero.shot(dt,hero);
+		hero.shot(dt, hero);
 		enemymanager.updateall(dt);
 		move = hero.getMoveSpeed() * dt;
 		controlHero(hero, canvas, move);
-		changemao(hero, mapoffestx,mapoffesty);
-		camera.SetPosition(hero.transform.GetPositionX()-(canvas.getWidth()/2), hero.transform.GetPositionY()-(canvas.getHeight()/2));
+		changemao(hero, mapoffestx, mapoffesty);
+		camera.SetPosition(hero.transform.GetPositionX() - (canvas.getWidth() / 2), hero.transform.GetPositionY() - (canvas.getHeight() / 2));
 		bulletmanager.updateAll(dt);
 		gameobjectmanager.UpdateAll(dt);
 		// Check for input (key presses or window events)
 		// Clear the window for the next frame rendering
 		canvas.clear();
-	
-		draw_entire_background(mapsave1, canvas, tiles, mapoffestx,mapoffesty,watermap);
+
+		draw_entire_background(mapsave1, canvas, tiles, mapoffestx, mapoffesty, watermap);
 		//draw_title((int)floorf(hero.transform.GetPositionX()), (int)floorf(hero.transform.GetPositionY()), canvas, hero.image);
-		draw_object( canvas, gameobjectmanager.getobjects(),gameobjectmanager.GetCount());
-		
+		draw_object(canvas, gameobjectmanager.getobjects(), gameobjectmanager.GetCount());
+
 		//draw_collision(hero, canvas);
-		
-		
+
+
 		// Update game logic
 		// Draw();
 		// Display the frame on the screen. This must be called once the frameis finished in order to display the frame.
-			canvas.present();
+		canvas.present();
 	}
 	return 0;
 }
-void changemao(Hero& hero, int** mapmapoffestx,int** mapmapoffesty) {
-	int countx = ((int)hero.transform.GetPositionX()- 672)/ (42*32);
+void changemao(Hero& hero, int** mapmapoffestx, int** mapmapoffesty) {
+	int countx = ((int)hero.transform.GetPositionX() - 672) / (42 * 32);
 	int county = ((int)hero.transform.GetPositionY() - 672) / (42 * 32);
-	int offestx = ((int)hero.transform.GetPositionX()- 672)  % (42 * 32) / 32;
+	int offestx = ((int)hero.transform.GetPositionX() - 672) % (42 * 32) / 32;
 	int offesty = ((int)hero.transform.GetPositionY() - 672) % (42 * 32) / 32;
 
 	for (int i = 0; i < 42; i++) {
@@ -192,15 +220,15 @@ void changemao(Hero& hero, int** mapmapoffestx,int** mapmapoffesty) {
 			mapmapoffesty[i][j] = 0;
 		}
 	}
-	if (offestx < 0||countx<0) {
-		
+	if (offestx < 0 || countx < 0) {
+
 		for (int i = 41; i >= 0; i--) {
-			for (int j = 41; j >=0; j--) {
+			for (int j = 41; j >= 0; j--) {
 				if (j > 41 + offestx) {
 					mapmapoffestx[j][i] = (-1 + countx * 1) * 42 * 32;
 				}
 				else {
-					mapmapoffestx[j][i] = ( countx * 1) * 42 * 32;
+					mapmapoffestx[j][i] = (countx * 1) * 42 * 32;
 				}
 			}
 		}
@@ -209,62 +237,62 @@ void changemao(Hero& hero, int** mapmapoffestx,int** mapmapoffesty) {
 		for (int i = 0; i < 42; i++) {
 			for (int j = 0; j < 42; j++) {
 
-				if(j<offestx)
-				mapmapoffestx[j][i] = (1 + countx * 1)*42*32;
+				if (j < offestx)
+					mapmapoffestx[j][i] = (1 + countx * 1) * 42 * 32;
 				else {
 					mapmapoffestx[j][i] = (countx * 1) * 42 * 32;
 				}
 			}
 		}
 	}
-	if (offesty >= 0 || county >0) {
+	if (offesty >= 0 || county > 0) {
 		for (int i = 0; i < 42; i++) {
 			for (int j = 0; j < 42; j++) {
 				if (j < offesty) {
 					mapmapoffesty[i][j] = (1 + county * 1) * 42 * 32;
 				}
 				else {
-					mapmapoffesty[i][j] = ( county * 1) * 42 * 32;
+					mapmapoffesty[i][j] = (county * 1) * 42 * 32;
 				}
 			}
 		}
 	}
 	if (offesty < 0 || county < 0) {
 		for (int i = 0; i < 42; i++) {
-			for (int j = 41; j >=0; j--) {
+			for (int j = 41; j >= 0; j--) {
 				if (j > 41 + offesty) {
 					mapmapoffesty[i][j] = (-1 + county * 1) * 42 * 32;
 				}
 				else {
-					mapmapoffesty[i][j] = ( county * 1) * 42 * 32;
+					mapmapoffesty[i][j] = (county * 1) * 42 * 32;
 				}
-				
+
 			}
 		}
 	}
-		
+
 }
 void draw_title(int x, int y, GamesEngineeringBase::Window& canvas, GamesEngineeringBase::Image& image) {//draw a pixil image at (x,y) position on the canvas
 
-	for ( int i = 0; i < image.height; i++) {
-		for ( int j = 0; j < image.width; j++) {
-			if(x + j  >=0&& i + y  >= 0 && x + j  <canvas.getWidth() && i + y <canvas.getHeight()) {
+	for (int i = 0; i < image.height; i++) {
+		for (int j = 0; j < image.width; j++) {
+			if (x + j >= 0 && i + y >= 0 && x + j < canvas.getWidth() && i + y < canvas.getHeight()) {
 				if (image.alphaAt(j, i)) {
-					canvas.draw(x + j , y + i , image.at(j, i));
+					canvas.draw(x + j, y + i, image.at(j, i));
 				}
 				//cout << i + x <<"   "<< j + y<<endl;//only a little test can be used
-				
+
 			}
-			
+
 		}
 	}
-	
+
 }
-void draw_object( GamesEngineeringBase::Window& canvas,GameObject** obj,int count) {//draw a pixil image at (x,y) position on the canvas
+void draw_object(GamesEngineeringBase::Window& canvas, GameObject** obj, int count) {//draw a pixil image at (x,y) position on the canvas
 
 	for (int o = 0; o < count; o++) {
 		if (obj[o]) {
-			
+
 			draw_collision(*obj[o], canvas);//draw collision box for testing
 			for (int i = 0; i < obj[o]->image.height; i++) {
 				for (int j = 0; j < obj[o]->image.width; j++) {
@@ -285,45 +313,45 @@ void draw_object( GamesEngineeringBase::Window& canvas,GameObject** obj,int coun
 
 
 }
-void camera_draw(float x,float y, GamesEngineeringBase::Image& image,GamesEngineeringBase::Window& canvas,GameObject* obj) {
-	if(obj->transform.GetPositionX() + x - Camera::GetCamera().GetX()>=0&& obj->transform.GetPositionX() + x - Camera::GetCamera().GetX()<canvas.getWidth()&& obj->transform.GetPositionY() + y - Camera::GetCamera().GetY()>=0&& obj->transform.GetPositionY() + y - Camera::GetCamera().GetY()<canvas.getHeight())
-	canvas.draw(obj->transform.GetPositionX() + x - Camera::GetCamera().GetX(), obj->transform.GetPositionY() + y - Camera::GetCamera().GetY(), obj->image.at(x, y));
+void camera_draw(float x, float y, GamesEngineeringBase::Image& image, GamesEngineeringBase::Window& canvas, GameObject* obj) {
+	if (obj->transform.GetPositionX() + x - Camera::GetCamera().GetX() >= 0 && obj->transform.GetPositionX() + x - Camera::GetCamera().GetX() < canvas.getWidth() && obj->transform.GetPositionY() + y - Camera::GetCamera().GetY() >= 0 && obj->transform.GetPositionY() + y - Camera::GetCamera().GetY() < canvas.getHeight())
+		canvas.draw(obj->transform.GetPositionX() + x - Camera::GetCamera().GetX(), obj->transform.GetPositionY() + y - Camera::GetCamera().GetY(), obj->image.at(x, y));
 }
 
 void draw_collision(GameObject& gameobject, GamesEngineeringBase::Window& canvas) {
-	for (int i = gameobject.collision.getcollisionX()-Camera::GetCamera().GetX(); i < gameobject.collision.getcollisionX()+ gameobject.collision.getcollisionW() - Camera::GetCamera().GetX(); i++) {
-		if (i >= 0 && gameobject.collision.getcollisionY() - Camera::GetCamera().GetY() >= 0 && i < canvas.getWidth() &&  gameobject.collision.getcollisionY() - Camera::GetCamera().GetY() < canvas.getHeight()) {
+	for (int i = gameobject.collision.getcollisionX() - Camera::GetCamera().GetX(); i < gameobject.collision.getcollisionX() + gameobject.collision.getcollisionW() - Camera::GetCamera().GetX(); i++) {
+		if (i >= 0 && gameobject.collision.getcollisionY() - Camera::GetCamera().GetY() >= 0 && i < canvas.getWidth() && gameobject.collision.getcollisionY() - Camera::GetCamera().GetY() < canvas.getHeight()) {
 			canvas.draw(i, gameobject.collision.getcollisionY() - Camera::GetCamera().GetY(), 0, 0, 225);
 
 		}
-		
+
 	}
 	for (int i = gameobject.collision.getcollisionX() - Camera::GetCamera().GetX(); i < gameobject.collision.getcollisionX() + gameobject.collision.getcollisionW() - Camera::GetCamera().GetX(); i++) {
 		if (i >= 0 && gameobject.collision.getcollisionY() + gameobject.collision.getcollisionH() - Camera::GetCamera().GetY() >= 0 && i < canvas.getWidth() && gameobject.collision.getcollisionY() + gameobject.collision.getcollisionH() - Camera::GetCamera().GetY() < canvas.getHeight()) {
-			canvas.draw(i, gameobject.collision.getcollisionY()+gameobject.collision.getcollisionH() - Camera::GetCamera().GetY(), 0, 0, 225);
+			canvas.draw(i, gameobject.collision.getcollisionY() + gameobject.collision.getcollisionH() - Camera::GetCamera().GetY(), 0, 0, 225);
 		}
 
 	}
-	
+
 	for (int i = gameobject.collision.getcollisionY() - Camera::GetCamera().GetY(); i < gameobject.collision.getcollisionY() + gameobject.collision.getcollisionH() - Camera::GetCamera().GetY(); i++) {
-		if (gameobject.collision.getcollisionX() - Camera::GetCamera().GetX() >= 0 && i  >= 0 && gameobject.collision.getcollisionX() - Camera::GetCamera().GetX() < canvas.getWidth() && i < canvas.getHeight()) {
+		if (gameobject.collision.getcollisionX() - Camera::GetCamera().GetX() >= 0 && i >= 0 && gameobject.collision.getcollisionX() - Camera::GetCamera().GetX() < canvas.getWidth() && i < canvas.getHeight()) {
 			canvas.draw(gameobject.collision.getcollisionX() - Camera::GetCamera().GetX(), i, 0, 0, 225);
 		}
-		
+
 	}
 	for (int i = gameobject.collision.getcollisionY() - Camera::GetCamera().GetY(); i < gameobject.collision.getcollisionY() + gameobject.collision.getcollisionH() - Camera::GetCamera().GetY(); i++) {
 		if (gameobject.collision.getcollisionX() + gameobject.collision.getcollisionW() - Camera::GetCamera().GetX() >= 0 && i >= 0 && gameobject.collision.getcollisionX() + gameobject.collision.getcollisionW() - Camera::GetCamera().GetX() < canvas.getWidth() && i < canvas.getHeight()) {
-			canvas.draw(gameobject.collision.getcollisionX()+gameobject.collision.getcollisionW() - Camera::GetCamera().GetX(), i, 0, 0, 225);
+			canvas.draw(gameobject.collision.getcollisionX() + gameobject.collision.getcollisionW() - Camera::GetCamera().GetX(), i, 0, 0, 225);
 		}
 
 	}
-	
+
 	scvtest = false;
 
 }
-void draw_entire_background(int **mapsave1, GamesEngineeringBase::Window& canvas, GamesEngineeringBase::Image* tiles,int** offestmapx,int** offestmapy,Water** watermap) {
+void draw_entire_background(int** mapsave1, GamesEngineeringBase::Window& canvas, GamesEngineeringBase::Image* tiles, int** offestmapx, int** offestmapy, Water** watermap) {
 	for (int i = 0; i < canvas.getHeight(); i++) {
-		for(int j=0;j< canvas.getWidth(); j++) {
+		for (int j = 0; j < canvas.getWidth(); j++) {
 			canvas.draw(j, i, 0, 0, 0);
 		}
 	}
@@ -331,33 +359,33 @@ void draw_entire_background(int **mapsave1, GamesEngineeringBase::Window& canvas
 	for (int i = 0; i < 42; i++) {
 		for (int j = 0; j < 42; j++) {
 			if (mapsave1[i][j] > -1 && mapsave1[i][j] < 24) {
-				if (mapsave1[i][j] >= 14&&mapsave1[i][j]<=22) {
+				if (mapsave1[i][j] >= 14 && mapsave1[i][j] <= 22) {
 					watermap[i][j].transform.SetPosition((j * 32) + offestmapx[j][i], (i * 32) + offestmapy[j][i]);
 					watermap[i][j].Update(0, Camera::GetCamera());
 				}
 
-				draw_title((j * 32)-Camera::GetCamera().GetX()+ offestmapx[j][i], (i * 32) - Camera::GetCamera().GetY()+ offestmapy[j][i], canvas, tiles[mapsave1[i][j]]);
-				
+				draw_title((j * 32) - Camera::GetCamera().GetX() + offestmapx[j][i], (i * 32) - Camera::GetCamera().GetY() + offestmapy[j][i], canvas, tiles[mapsave1[i][j]]);
+
 			}
 		}
 
 
 	}
 }
-void controlHero(Hero& hero, GamesEngineeringBase::Window& canvas,float move) {
-	
+void controlHero(Hero& hero, GamesEngineeringBase::Window& canvas, float move) {
+
 
 	if (canvas.keyPressed('W')) {
-		cout << hero.transform.GetPositionY()<<endl;
-		
-		hero.transform.SetPosition(hero.transform.GetPositionX(),hero.transform.GetPositionY() - move);
+		cout << hero.transform.GetPositionY() << endl;
+
+		hero.transform.SetPosition(hero.transform.GetPositionX(), hero.transform.GetPositionY() - move);
 		hero.Update();
 		if (GameObjectManager::getInstance().checkwater()) {
 			hero.transform.SetPosition(hero.transform.GetPositionX(), hero.transform.GetPositionY() + move);
 			hero.Update();
-			cout << "what happend" << "   " << hero.transform.GetPositionY() << "   " << hero.transform.GetPositionY()<<endl;
+			cout << "what happend" << "   " << hero.transform.GetPositionY() << "   " << hero.transform.GetPositionY() << endl;
 		}
-		
+
 
 	}
 	if (canvas.keyPressed('S')) {
@@ -368,11 +396,11 @@ void controlHero(Hero& hero, GamesEngineeringBase::Window& canvas,float move) {
 			hero.Update();
 			cout << "what happend" << "   " << hero.transform.GetPositionY() << "   " << hero.transform.GetPositionY() << endl;
 		}
-		
+
 
 	}
 	if (canvas.keyPressed('A')) {
-		hero.transform.SetPosition(hero.transform.GetPositionX()-move, hero.transform.GetPositionY() );
+		hero.transform.SetPosition(hero.transform.GetPositionX() - move, hero.transform.GetPositionY());
 		hero.Update();
 		if (GameObjectManager::getInstance().checkwater()) {
 			hero.transform.SetPosition(hero.transform.GetPositionX() + move, hero.transform.GetPositionY());
@@ -381,7 +409,7 @@ void controlHero(Hero& hero, GamesEngineeringBase::Window& canvas,float move) {
 
 	}
 	if (canvas.keyPressed('D')) {
-		hero.transform.SetPosition(hero.transform.GetPositionX()+ move, hero.transform.GetPositionY());
+		hero.transform.SetPosition(hero.transform.GetPositionX() + move, hero.transform.GetPositionY());
 		hero.Update();
 		if (GameObjectManager::getInstance().checkwater()) {
 			hero.transform.SetPosition(hero.transform.GetPositionX() - move, hero.transform.GetPositionY());
@@ -394,6 +422,6 @@ void controlHero(Hero& hero, GamesEngineeringBase::Window& canvas,float move) {
 			enemiesmanager::getInstance().killsomeenemies(Hero::getInstance().getabilitymax());
 			Hero::getInstance().setabilitytimecount(0.f);
 		}
-		
+
 	}
 }
